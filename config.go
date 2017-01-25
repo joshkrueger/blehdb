947ac10ea026397ac88d228baaa93e6f0bd7f39a
Josh Krueger <joshsinbox@gmail.com>
2017-01-25 13:04:47 -0600

    Implements config

---
+++ config.go
@@ -0,0 +1,31 @@
+package blehdb
+
+import "fmt"
+
+// Config provides the necessary configuration to the BlehDB server.
+type Config struct {
+	// StorageDir specifies the base path for persisting data on disk. The server
+	// will attempt to create this path if it does not already exist.
+	StorageDir string
+
+	// RaftBind specifies the bind address for the Raft server.
+	RaftBind string
+
+	// RPCBind specifies the bind address for the RPC server.
+	RPCBind string
+}
+
+func DefaultConfig() *Config {
+	return &Config{
+		RaftBind: ":11000",
+		RPCBind:  ":12000",
+	}
+}
+
+func ValidateConfig(config *config) error {
+	if config.StorageDir == "" {
+		return fmt.Errorf("A StorageDir must be specified")
+	}
+
+	return nil
+}
